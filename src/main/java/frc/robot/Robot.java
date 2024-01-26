ec33072087e2fed56ab901bcfddd54bb628b1e71
Total57 <98564394+Total57@users.noreply.github.com>
2024-01-25 18:27:23 -0600

    encoder no terminado

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -6,9 +6,11 @@ import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import com.revrobotics.RelativeEncoder;
+import com.revrobotics.AbsoluteEncoder;
 import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
+import com.revrobotics.AnalogInput;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
@@ -18,35 +20,55 @@ public class Robot extends TimedRobot {
 
   private final Joystick m_stick = new Joystick(0);
 
-  DutyCycleEncoder encoder = new DutyCycleEncoder(0);
+  public class AnalogInput {
+  private final AnalogInput absoluteEncoder;
+
+  public AnalogInput getAbsoluteEncoder() {
+
+    return absoluteEncoder;
+  }
+
+  public AnalogInput(AnalogInput absoluteEncoder) {
+    this.absoluteEncoder = absoluteEncoder;
+    absoluteEncoder = new AnalogInput(absoluteEncoder);
+  } 
+  }
+
+
 
   private RelativeEncoder encoder_r ;
   private RelativeEncoder encoder_l ;
 
-  public Robot() { }
 
-  @Override
-  public void robotInit() { }
+  public Robot() {
+
+   }
+
+  
+   @Override
+  public void robotInit() {
+  
+   }
 
   @Override
   public void teleopPeriodic() {
+ 
     double axis1 = m_stick.getRawAxis(1);
     double axis5 = m_stick.getRawAxis(5);
 
     m_leftMotor.set(axis1);
-    m_rightMotor.set(axis5);
-
-    encoder.setDistancePerRotation(4.0);
-    double v_encoder = encoder.getDistance(); 
+    m_rightMotor.set(axis5); 
      
     encoder_r = m_leftMotor.getEncoder();
     encoder_l = m_rightMotor.getEncoder();
 
+    double angle = absoluteEncoder.getVoltage();
+
     SmartDashboard.putNumber("Encoder motor derecho", encoder_r.getPosition());
     SmartDashboard.putNumber("Encoder motor izquierdo", encoder_l.getPosition());
 
-    SmartDashboard.putNumber("Encoder externo", v_encoder);
+    //SmartDashboard.putNumber("Encoder externo", );
 
-    //hola
+
   }
 }
