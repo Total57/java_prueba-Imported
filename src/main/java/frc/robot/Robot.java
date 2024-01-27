f10e05dc0bc81e22c58b484ad7c9e40882110eba
Total57 <98564394+Total57@users.noreply.github.com>
2024-01-27 09:40:40 -0600

    CODIGO vierned

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,18 +1,15 @@
+
 package frc.robot;
 
-import edu.wpi.first.util.sendable.SendableRegistry;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+import com.revrobotics.CANSparkMax;
+
+import edu.wpi.first.wpilibj.AnalogPotentiometer;
 import edu.wpi.first.wpilibj.DutyCycleEncoder;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import com.revrobotics.RelativeEncoder;
-import com.revrobotics.AbsoluteEncoder;
-import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-import com.revrobotics.AnalogInput;
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkLowLevel.MotorType;
 
 public class Robot extends TimedRobot {
   private final CANSparkMax m_leftMotor = new CANSparkMax(1, MotorType.kBrushless);
@@ -20,53 +17,32 @@ public class Robot extends TimedRobot {
 
   private final Joystick m_stick = new Joystick(0);
 
-  public class AnalogInput {
-  private final AnalogInput absoluteEncoder;
-
-  public AnalogInput getAbsoluteEncoder() {
-
-    return absoluteEncoder;
-  }
-
-  public AnalogInput(AnalogInput absoluteEncoder) {
-    this.absoluteEncoder = absoluteEncoder;
-    absoluteEncoder = new AnalogInput(absoluteEncoder);
-  } 
-  }
+  DutyCycleEncoder encodere = new DutyCycleEncoder(0);
   
-  private RelativeEncoder encoder_r ;
-  private RelativeEncoder encoder_l ;
-
-
-  public Robot() {
-
-   }
-
+  AnalogPotentiometer encoderExterno = new AnalogPotentiometer(0,180,30);
   
-   @Override
-  public void robotInit() {
-  
-   }
+  public Robot() { }
+
+  @Override
+  public void robotInit() { }
 
   @Override
   public void teleopPeriodic() {
- 
     double axis1 = m_stick.getRawAxis(1);
     double axis5 = m_stick.getRawAxis(5);
 
     m_leftMotor.set(axis1);
-    m_rightMotor.set(axis5); 
-     
-    encoder_r = m_leftMotor.getEncoder();
-    encoder_l = m_rightMotor.getEncoder();
+    m_rightMotor.set(axis5);
 
-    double angle = absoluteEncoder.getVoltage();
-
-    SmartDashboard.putNumber("Encoder motor derecho", encoder_r.getPosition());
-    SmartDashboard.putNumber("Encoder motor izquierdo", encoder_l.getPosition());
-
-    //SmartDashboard.putNumber("Encoder externo", );
-
-
-  }
-}
+    encodere.setDistancePerRotation(5.0);
+    encodere.getDistance();
+    //robles
+    encodere.close();
+ 
+      double enc = encoderExterno.get();
+      encoderExterno.close();
+   
+    SmartDashboard.putNumber("Encoder", enc);
+      
+    
+}}
